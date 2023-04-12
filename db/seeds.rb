c8629ad6f017d9929d0994f07f964d5f7f85ff1d
Kervin Vasquez <kervinlvh@gmail.com>
2023-04-11 22:51:47 -0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+# Clear out the database
+Address.destroy_all
+YardSale.destroy_all
+User.destroy_all
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -36,49 +41,45 @@ user3 =
   )
 
 # create some yardsales for each user
-user1.yard_sales.create(
+user1.yard_sales.create!(
   title: "Awesome Stuff this Saturday",
   description: "Lots of cool stuff",
   date: "Tue, 08 Dec 2015",
-  hours: "Sat, 01 Jan 2000 12:26:40.000000000 UTC +00:00"
+  hours: "Sat, 01 Jan 2000 12:26:40.000000000 UTC +00:00",
+  address:
+    Address.create(
+      address: "12311 Exact Staff st",
+      city: "Calabasas",
+      zip_code: "12333",
+      state: "CA",
+      country: "USA"
+    )
 )
-user2.yard_sales.create(
+user2.yard_sales.create!(
   title: " Sunday",
   description: "Lots of cool stuff",
   date: "Tue, 08 Dec 2015",
-  hours: "Sat, 01 Jan 2000 12:26:40.000000000 UTC +00:00"
+  hours: "Sat, 01 Jan 2000 12:26:40.000000000 UTC +00:00",
+  address:
+    Address.create(
+      address: "12311 Exact Staff st",
+      city: "Calabasas",
+      zip_code: "12333",
+      state: "CA",
+      country: "USA"
+    )
 )
-user3.yard_sales.create(
+user3.yard_sales.create!(
   title: "Monday",
   description: "Lots of cool stuff",
   date: "Tue, 08 Dec 2015",
-  hours: "Sat, 01 Jan 2000 12:26:40.000000000 UTC +00:00"
-)
-
-# create some addresses for each yardsale
-user1.yard_sales.addresses.create(
-  address: "12345 Exact Staff st",
-  city: "Calabasas",
-  zip_code: "12343",
-  state: "CA",
-  country: "USA",
-  yard_sale_id: 21
-)
-
-user2.yard_sales.addresses.create(
-  address: "12311 Exact Staff st",
-  city: "Calabasas",
-  zip_code: "12333",
-  state: "CA",
-  country: "USA",
-  yard_sale_id: 22
-)
-
-user3.yard_sales.addresses.create(
-  address: "12111 Exact Staff st",
-  city: "Calabasas",
-  zip_code: "12789",
-  state: "CA",
-  country: "USA",
-  yard_sale_id: 23
+  hours: "Sat, 01 Jan 2000 12:26:40.000000000 UTC +00:00",
+  address:
+    Address.create(
+      address: "12311 Exact Staff st",
+      city: "Calabasas",
+      zip_code: "12333",
+      state: "CA",
+      country: "USA"
+    )
 )
