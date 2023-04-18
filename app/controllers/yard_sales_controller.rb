eeeb1c1352221eeea8d900367740f6a6da3e6e81
Daniel Jimenez <djimenez017@gmail.com>
2023-04-18 01:26:49 -0700

    working on single page view. need to work on responsiveness, and figuring out how to add tags to the seed file to test the entire thing

--- app/controllers/yard_sales_controller.rb
+++ app/controllers/yard_sales_controller.rb
@@ -1,4 +1,5 @@
 class YardSalesController < ApplicationController
   def show
+    @yardsale = YardSale.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "pages#home"
-  resources :yardsales
+  get "/yardsales/:id", to: "yard_sales#show", as: "yardsale"
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
